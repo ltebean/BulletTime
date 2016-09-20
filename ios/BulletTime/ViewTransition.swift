bc910adfc0c25d867af91ab7d972509ce59f5b26
ltebean <yucong1118@gmail.com>
2016-09-20 18:02:27 +0800

    refactor

--- ios/BulletTime/ViewTransition.swift
+++ ios/BulletTime/ViewTransition.swift
@@ -11,23 +11,8 @@ import Async
 
 class AnimatableViewController: UIViewController {
     
-    var isPush = false
-    
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-//        let views = viewsToAnimate()
-//        UIView.animateWithDuration(0.3, delay: 0, options: [.CurveEaseOut], animations: {
-//            views.forEach {
-//                $0.alpha = 1
-//                $0.transform.ty = 0
-//            }
-//            self.view.backgroundColor = self.backgroundColor()
-//        }, completion: nil)
-    }
-    
     override func viewWillDisappear(animated: Bool) {
         super.viewWillDisappear(animated)
-
     }
     
     func viewsToAnimate() -> [UIView] {
@@ -54,7 +39,6 @@ class AnimatableViewController: UIViewController {
             self.view.backgroundColor = self.backgroundColor()
             self.navigationController?.pushViewController(viewController, animated: true)
         })
-   
     }
     
     func pop() {
@@ -75,7 +59,6 @@ class AnimatableViewController: UIViewController {
 
 class ViewTransitionDelegate: NSObject, UINavigationControllerDelegate {
     
-    var interactiveTransition: UIPercentDrivenInteractiveTransition?
     let transition: ViewTransition
     let navigationController: UINavigationController
     
@@ -86,7 +69,6 @@ class ViewTransitionDelegate: NSObject, UINavigationControllerDelegate {
     
     func navigationController(navigationController: UINavigationController, animationControllerForOperation operation: UINavigationControllerOperation, fromViewController fromVC: UIViewController, toViewController toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         transition.push = operation == UINavigationControllerOperation.Push
-        transition.delegate = self
         return transition
     }
     
@@ -104,7 +86,6 @@ class ViewTransition: NSObject, UIViewControllerAnimatedTransitioning, UIGesture
     }
     
     var push = true
-    weak var delegate: ViewTransitionDelegate!
     
     func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
         return 0.35
@@ -114,21 +95,16 @@ class ViewTransition: NSObject, UIViewControllerAnimatedTransitioning, UIGesture
         
         let fromVC = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)! as! AnimatableViewController
         let toVC = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)! as! AnimatableViewController
-        fromVC.isPush = push
-        toVC.isPush = !push
         
         let toView = toVC.view
-        
         let containerView = transitionContext.containerView()
-        
+        containerView.addSubview(toView)
+
         let duration = transitionDuration(transitionContext)
         
         if (push) {
             handleBack(toVC)
         }
-        
-        containerView.addSubview(toView)
-        
         
         let views = toVC.viewsToAnimate()
         
@@ -136,27 +112,19 @@ class ViewTransition: NSObject, UIViewControllerAnimatedTransitioning, UIGesture
             $0.alpha = 0
             $0.transform.ty = push ? 60 : -60
         }
+        
         UIView.animateWithDuration(duration,  delay: 0, options: [.CurveEaseOut], animations: {
             views.forEach {
                 $0.alpha = 1
                 $0.transform.ty = 0
             }
         }, completion: { finished in
-    
             transitionContext.completeTransition(!transitionContext.transitionWasCancelled())
-
         })
     
-        
-        
-        
     }
     
     func handleBack(vc: AnimatableViewController) {
-//        let gesture = PanDirectionGestureRecognizer(direction: .Vertical, target: self, action: #selector(Transition.handlePan))
-//        //        gesture.delegate = self
-//        vc.view.addGestureRecognizer(gesture)
-//        
         let gesture = UISwipeGestureRecognizer(target: self, action: #selector(ViewTransition.handleSwipe))
         gesture.direction = .Down
         vc.view.addGestureRecognizer(gesture)
@@ -166,28 +134,6 @@ class ViewTransition: NSObject, UIViewControllerAnimatedTransitioning, UIGesture
         if gesture.state == .Ended {
             let vc = navigationController.topViewController as! AnimatableViewController
             vc.pop()
-        }
-    }
-    
-    
-    func handlePan(gesture: UIPanGestureRecognizer) {
-        let progress = gesture.translationInView(gesture.view).y / UIScreen.mainScreen().bounds.height
-        
-        if gesture.state == .Began {
-            delegate.interactiveTransition = UIPercentDrivenInteractiveTransition()
-            let vc = navigationController.topViewController as! AnimatableViewController
-            vc.pop()
-        }
-        else if gesture.state == .Changed {
-            delegate.interactiveTransition?.updateInteractiveTransition(progress)
-        }
-        else if gesture.state == .Ended || gesture.state == .Cancelled {
-            if progress > 0.3 {
-                delegate.interactiveTransition?.finishInteractiveTransition()
-            } else {
-                delegate.interactiveTransition?.cancelInteractiveTransition()
-            }
-            delegate.interactiveTransition = nil
         }
     }
     
